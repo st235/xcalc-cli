2ecc7338e6d603e1c66f00c34be856a824c8eb74
Alexander Dadukin <hi@st235.xyz>
2020-08-21 23:03:48 +0300

    updated core library.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,68 @@
 #include <string>
 #include <iostream>
+#include <unordered_map>
 #include "./libs/xcalc/include/xcalc.h"
 
-int main() {
+namespace  {
+
+bool ContainsKey(const std::unordered_map<std::string, std::string>& args, const std::string& arg) {
+    return args.find(arg) != args.end();
+}
+
+std::string GetOrDefault(std::unordered_map<std::string, std::string>& args, const std::string& arg, const std::string& def) {
+    if (!ContainsKey(args, arg)) {
+        return def;
+    }
+
+    return args[arg];
+}
+
+void PrintHelp() {
+    std::cout << "-A angle mode, could be: RADS (stands for radians), DEG (stands for degrees)" << std::endl
+              << "-e evaluate given expression" << std::endl
+              << "-h help" << std::endl;
+}
+
+xcalc::DegreeMode ToDegreeMode(const std::string& mode) {
+    if (mode == "RADS") {
+        return xcalc::DegreeMode::RADS;
+    }
+
+    return xcalc::DegreeMode::DEG;
+}
+
+}
+
+int main(int argc, char* argv[]) {
+    std::unordered_map<std::string, std::string> args;
+
+    for (int i = 1; i < argc; i++) {
+        std::string arg(argv[i]);
+
+        if (arg == "-h") {
+            args[arg] = "";
+            continue;
+        }
+
+        if (i < argc - 1) {
+            std::string nextArg(argv[i + 1]);
+            args[arg] = nextArg;
+            i++;
+        }
+    }
+
+    if (ContainsKey(args, "-h")) {
+        PrintHelp();
+        return 0;
+    }
+
+    xcalc::DegreeMode degreeMode = ToDegreeMode(GetOrDefault(args, "-A", "DEG"));
+
+    if (ContainsKey(args, "-e")) {
+        std::cout << xcalc::evaluate(degreeMode, GetOrDefault(args, "-e", "")) << std::endl;
+        return 0;
+    }
+
     std::cout << "Welcome at xcalc command line interface: " << std::endl;
     std::string user_input = "";
     while (true) {
@@ -14,8 +74,9 @@ int main() {
             break;
         }
 
-        std::cout << xcalc::evaluate(user_input) << std::endl;
+        std::cout << xcalc::evaluate(degreeMode, user_input) << std::endl;
     }
 
     return 0;
 }
+
